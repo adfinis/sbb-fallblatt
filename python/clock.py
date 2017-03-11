74b58053f4064916d4e1992034853003124e094e
Cyrill von Wattenwyl <eni@e23.ch>
2017-03-11 23:43:56 +0100

    clock with addr example

--- python/clock.py
+++ python/clock.py
@@ -5,11 +5,14 @@ import sbb_rs485
 import time
 from datetime import datetime
 
-
-
+SBB_MODULE_ADDR_HOUR = 82
+SBB_MODULE_ADDR_MIN  = 29
 
 def main():
-    clock = sbb_rs485.PanelClockControl()
+    clock = sbb_rs485.PanelClockControl(
+        addr_hour = SBB_MODULE_ADDR_HOUR,
+        addr_min  = SBB_MODULE_ADDR_MIN
+        )
     clock.connect()
     while True:
         clock.set_time_now()
