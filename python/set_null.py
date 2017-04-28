d8a59069ec5e28e2d4ce6ddb24e4b699801fa7c5
Cyrillvw <cyrill.vonwattenwyl@adfinis-sygroup.ch>
2017-04-28 13:58:47 +0200

    set null script

---
+++ python/set_null.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sbb_rs485
+import time
+from datetime import datetime
+
+
+def main():
+    cc = sbb_rs485.PanelClockControl()
+    cc.connect()
+    cc.serial.timeout = 2
+    exit = False
+
+    input("Press any key")
+    msg =  b"\xFF"
+    msg += CMD_ZERO
+    cc.send_msg(msg)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
