24e54e7b4c399ab59db280dc20a0957d129402fe
Michael Hofer <michael.hofer@adfinis.com>
2022-10-28 08:08:12 +0200

    fix(setup.py): use correct license, fix url and author information

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import os
 from setuptools import setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
@@ -10,17 +12,17 @@ with open("requirements.txt", encoding="utf-8") as f:
 setup(
     name="sbb_fallblatt",
     version="0.0.0",
-    author="Adfinis SyGroup",
-    author_email="info@adfinis-sygroup.ch",
+    author="Adfinis",
+    author_email="support@adfinis.com",
     description="Code and documentation for SBB split flap displays",
-    license="MIT",
+    license="GNU General Public License v3 (GPLv3)",
     keywords="sbb train display",
-    url="https://github.com/eni23/sbb-fallblatt",
+    url="https://github.com/adfinis/sbb-fallblatt",
     packages=['sbb_fallblatt'],
     long_description=read('README.md'),
     install_requires=requirements,
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
 )
